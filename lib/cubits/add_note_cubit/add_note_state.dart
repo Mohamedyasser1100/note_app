1d8fc7a70cf636a64970a5cabe0e63a171f6e14b
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2024-09-10 18:33:22 +0300

    add add note stats

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -4,3 +4,13 @@ part of 'add_note_cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSucces extends AddNoteState {}
+
+final class AddNoteFauiler extends AddNoteState {
+  final String errMessage;
+
+  AddNoteFauiler(this.errMessage);
+}
